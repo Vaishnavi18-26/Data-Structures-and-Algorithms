f3c7572e9161f537d30b90247ff2d2702d2d8ddb
Vaishnavi <vaishnaviofficial1803@gmail.com>
2026-03-08 23:48:42 +0530

    Add BubbleSort class with sorting implementation
    Implement Bubble Sort algorithm to sort an array.

---
+++ SearchingSorting/BubbleSort.java
@@ -0,0 +1,36 @@
+/*
+Problem:
+Sort an array using Bubble Sort.
+
+Time Complexity: O(n^2)
+*/
+
+public class BubbleSort {
+
+    static void bubbleSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5,3,8,2,1};
+
+        bubbleSort(arr);
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
