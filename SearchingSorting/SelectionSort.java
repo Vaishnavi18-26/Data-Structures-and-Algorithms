40917f41d838d5cc8ba6132e00cc253964994d2d
Vaishnavi <vaishnaviofficial1803@gmail.com>
2026-03-08 23:49:22 +0530

    Add Selection Sort implementation in Java
    Implemented Selection Sort algorithm to sort an array.

---
+++ SearchingSorting/SelectionSort.java
@@ -0,0 +1,37 @@
+/*
+Problem:
+Sort array using Selection Sort.
+
+Time Complexity: O(n^2)
+*/
+
+public class SelectionSort {
+
+    static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[j] < arr[min])
+                    min = j;
+            }
+
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {9,4,7,1,3};
+
+        selectionSort(arr);
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
