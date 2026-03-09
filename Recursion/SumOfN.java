54c751a3c1d158737f8f3f7b21408c7bf25b0c2e
Vaishnavi <vaishnaviofficial1803@gmail.com>
2026-03-09 23:56:41 +0530

    Add recursive sum function for first N natural numbers
    Implement recursive function to calculate sum of first N natural numbers.

---
+++ Recursion/SumOfN.java
@@ -0,0 +1,21 @@
+/*
+Problem:
+Find sum of first N natural numbers using recursion.
+*/
+
+public class SumOfN {
+
+    static int sum(int n) {
+
+        if (n == 0)
+            return 0;
+
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int n = 5;
+        System.out.println(sum(n));
+    }
+}
